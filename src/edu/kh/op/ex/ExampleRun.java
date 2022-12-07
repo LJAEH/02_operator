68c1779b50a5bf03b162810c35852ede3ae3a64c
LJAEH <wogurdlek33@gmail.com>
2022-12-07 14:27:30 +0900

    FirstCommit

---
+++ src/edu/kh/op/ex/ExampleRun.java
@@ -0,0 +1,21 @@
+package edu.kh.op.ex;
+
+// 메인 메소드 필수 작성 : 코드 실행용 클래스
+public class ExampleRun {
+	//메인 메소드 필수 작성
+	public static void main(String[] args) {
+
+		OpExample ex = new OpExample(); 
+		
+		// OpExample 이라는 설계도를 보고 
+		// OpExample 형태의 ex를 만들어냄.
+		
+		//ex.ex1(); 
+		// crtl ex1 누르면 원문으로 돌아감 
+		// 여기서 ex는 같은 패키지내에 존재해서 import가 필요하지않음
+		
+		ex.ex2();
+	}
+
+	
+}
